28139ff55595d4b35761b8911a51fadad8712315
Andrea Martinelli <andmartinelli@gmail.com>
2016-09-09 08:39:27 +0200

    Moved TypeScriptOutputVisitor out of NRefactory

---
+++ ICSharpCode.Decompiler/TypeScript/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Netjs
+{
+    public class App
+    {
+        public static void Warning(string v, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
